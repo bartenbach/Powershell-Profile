165aea0268a445125e7211b8d8ed93b5e60a4501
Blake Bartenbach <blakebartenbach@gmail.com>
2017-02-18 11:08:17 -0600

    + Updated profile with touch and new things + Added Microsoft vimrc

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,30 +5,42 @@
 Import-Module PSColor
 
 # PSDrives
-New-PSDrive -Name Code -PSProvider FileSystem -Root C:\Users\alureon\Documents\code >> $null
-New-PSDrive -Name Startup -PSProvider FileSystem -Root "C:\Users\alureon\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" >> $null
+New-PSDrive -Name Code      -PSProvider FileSystem -Root C:\Users\alureon\Documents\code >> $null
+New-PSDrive -Name plugins   -PSProvider FileSystem -Root C:\Users\alureon\minecraft\plugins >> $null
+New-PSDrive -Name minecraft -PSProvider FileSystem -Root C:\Users\alureon\minecraft >> $null
+New-PSDrive -Name idea      -PSProvider FileSystem -Root C:\Users\alureon\IdeaProjects >> $null
+New-PSDrive -Name Startup   -PSProvider FileSystem -Root "C:\Users\alureon\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" >> $null
 
 # Aliases
-New-Alias -Name npp -Value 'C:\Program Files (x86)\Notepad++\notepad++.exe' >> $null
-New-Alias -Name scrot -Value 'snippingtool'
+New-Alias -Name npp            -Value 'C:\Program Files (x86)\Notepad++\notepad++.exe' >> $null
+New-Alias -Name scrot          -Value 'snippingtool'
+New-Alias -Name gradle         -Value 'C:\Users\alureon\Downloads\gradle-3.0-bin\gradle-3.0\bin\gradle.bat'
+New-Alias -Name pc-information -Value 'C:\Users\alureon\Documents\code\PC-Information\PC-Information.ps1'
+
+# Set error background color to current background color
+$host.PrivateData.ErrorBackgroundColor = $host.UI.RawUI.BackgroundColor
 
 # Prompt
 function Prompt {
-    Write-Host "[" -ForegroundColor "Green" -NoNewLine
-    Write-Host $env:username -ForegroundColor "Green" -NoNewLine
-    Write-Host "@" -ForegroundColor "Green" -NoNewLine
-    Write-Host $env:computername -ForegroundColor "Green" -NoNewLine
-    Write-Host "] " -ForegroundColor "Green" -NoNewLine
-    Write-Host (shorten-path (pwd).Path) -ForegroundColor "Green" -NoNewLine
-    Write-Host " % " -ForegroundColor "Green" -NoNewLine
-    return ' '
+  Write-Host "[" -ForegroundColor "Green" -NoNewLine
+  Write-Host $env:username -ForegroundColor "Green" -NoNewLine
+  Write-Host "@" -ForegroundColor "Green" -NoNewLine
+  Write-Host $env:computername -ForegroundColor "Green" -NoNewLine
+  Write-Host "] " -ForegroundColor "Green" -NoNewLine
+  Write-Host (pwd | select-object Path -expandproperty path) -ForegroundColor "Green" -NoNewLine
+  Write-Host " % " -ForegroundColor "Green" -NoNewLine
+  return ' '
 }
 
-function shorten-path([string] $path) {
-   $loc = $path.Replace($HOME, '~')
-   # remove prefix for UNC paths
-   $loc = $loc -replace '^[^:]+::', ''
-   # make path shorter like tabs in Vim,
-   # handle paths starting with \\ and . correctly
-   return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2')
+# Touch
+function touch {
+  $file = $args[0]
+  if ($file -eq $null) {
+    throw "No filename specified"
+  }
+  if (Test-Path $file) {
+    (gci $file).LastWriteTime = Get-Date
+  } else {
+    New-Item -ItemType file $file
+  }
 }
