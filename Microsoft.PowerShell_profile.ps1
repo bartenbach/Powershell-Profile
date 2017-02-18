cfbe4b68becc514ede26b2d6c328b071ac3f32c0
Blake Bartenbach <blakebartenbach@gmail.com>
2017-02-18 12:43:38 -0600

    + Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,12 +10,13 @@ New-PSDrive -Name plugins   -PSProvider FileSystem -Root C:\Users\alureon\minecr
 New-PSDrive -Name minecraft -PSProvider FileSystem -Root C:\Users\alureon\minecraft >> $null
 New-PSDrive -Name idea      -PSProvider FileSystem -Root C:\Users\alureon\IdeaProjects >> $null
 New-PSDrive -Name Startup   -PSProvider FileSystem -Root "C:\Users\alureon\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" >> $null
+New-PSDrive -Name vim       -PSProvider FileSystem -Root "C:\Program Files (x86)\Vim" >> $null
 
 # Aliases
 New-Alias -Name npp            -Value 'C:\Program Files (x86)\Notepad++\notepad++.exe' >> $null
 New-Alias -Name scrot          -Value 'snippingtool'
 New-Alias -Name gradle         -Value 'C:\Users\alureon\Downloads\gradle-3.0-bin\gradle-3.0\bin\gradle.bat'
-New-Alias -Name pc-information -Value 'C:\Users\alureon\Documents\code\PC-Information\PC-Information.ps1'
+New-Alias -Name pc-information -Value 'C:\Users\alureon\Documents\code\PC-Information\Get-PC-Information.ps1'
 
 # Set error background color to current background color
 $host.PrivateData.ErrorBackgroundColor = $host.UI.RawUI.BackgroundColor
@@ -28,7 +29,7 @@ function Prompt {
   Write-Host $env:computername -ForegroundColor "Green" -NoNewLine
   Write-Host "] " -ForegroundColor "Green" -NoNewLine
   Write-Host (pwd | select-object Path -expandproperty path) -ForegroundColor "Green" -NoNewLine
-  Write-Host " % " -ForegroundColor "Green" -NoNewLine
+  Write-Host " %" -ForegroundColor "Green" -NoNewLine
   return ' '
 }
 
@@ -44,3 +45,4 @@ function touch {
     New-Item -ItemType file $file
   }
 }
+
